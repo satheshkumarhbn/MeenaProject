dfbfae767094b3d191e66534785f49c707672334
Meena <meenaswt3@gmail.com>
2017-12-22 15:44:36 +0530

    Add 1 class

--- junitproject/src/main/java/com/ibm/junitproject/ClassBaby.java
+++ junitproject/src/main/java/com/ibm/junitproject/ClassBaby.java
@@ -1,7 +1,7 @@
 package com.ibm.junitproject;
 
 public class ClassBaby {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
 		System.out.println("Hai Baby");
 	}

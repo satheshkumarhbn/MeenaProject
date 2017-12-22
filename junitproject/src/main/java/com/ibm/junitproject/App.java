0029aee47237f1f5e472357ee3cb2f7437327d81
Sathesh <contactstheshhbn@gmail.com>
2017-12-22 15:05:04 +0530

    initial commit

---
+++ junitproject/src/main/java/com/ibm/junitproject/App.java
@@ -0,0 +1,17 @@
+package com.ibm.junitproject;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String a = StringUtils.capitalize("sdfssdbfhks");
+        System.out.println(a);
+    }
+}
